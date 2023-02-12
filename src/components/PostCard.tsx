5b639b984050f5b4b46a1a0a6f58fa79e19d167d
h.belgacem <hassene@belgacem.io>
2023-02-12 21:13:24 +0100

    Add separate icon for each post

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -18,10 +18,10 @@ const PostCard: React.FC<Props> = ({ post }) => {
           className="flex flex-col md:flex-row overflow-hidden mb-6 md:mb-8 rounded-2xl bg-white dark:bg-zinc-700 hover:shadow-lg transition-shadow "
         >
           {post.thumbnail && (
-            <div className="relative w-full bg-gray-200 dark:bg-zinc-700">
+            <div className="relative md:w-120 bg-gray-200 dark:bg-zinc-700">
               <Image
                 src={post.thumbnail}
-                className="w-full object-contain md:h-full md:w-48"
+                className="w-full object-fill md:h-full md:w-48"
                 alt={post.title}
               />
             </div>
